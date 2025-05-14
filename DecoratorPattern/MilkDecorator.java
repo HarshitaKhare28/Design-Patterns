3119f09ddd76f4475655f474a9670eb00bf37738
Harshita Khare <121930563+HarshitaKhare28@users.noreply.github.com>
2025-05-14 17:02:47 +0530

    Added Decorator Pattern

---
+++ DecoratorPattern/MilkDecorator.java
@@ -0,0 +1,13 @@
+public class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    public String getDescription() {
+        return decoratedCoffee.getDescription() + ", Milk";
+    }
+
+    public double getCost() {
+        return decoratedCoffee.getCost() + 1.5;
+    }
+}
---
+++ DecoratorPattern/SimpleCoffee.java
@@ -0,0 +1,10 @@
+// SimpleCoffee.java
+public class SimpleCoffee implements Coffee {
+    public String getDescription() {
+        return "Simple Coffee";
+    }
+
+    public double getCost() {
+        return 5.0;
+    }
+}
